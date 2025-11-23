cf4d302c2aed2eb823470e80265d5120ffd2abb3
MIStatlE <24110980015@m.fudan.edu.cn>
2025-11-23 12:57:02 +0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', // 关键：确保在 GitHub Pages 上的路径正确
-})
